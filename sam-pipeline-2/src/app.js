14a9fdfc8d4f60ba0961c46b0774d577448a47e7
vietha <ha.tv@briswell-vn.com>
2022-12-30 09:47:07 +0700

    update code to test

--- sam-pipeline-2/src/app.js
+++ sam-pipeline-2/src/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'ha test 123',
+                message: 'ha test 2',
                 // location: ret.data.trim()
             })
         }
